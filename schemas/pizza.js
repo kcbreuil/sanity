042e3018111dba8d752928a7021e5de28ee8777d
Katie Breuil <kcbreuil@gmail.com>
2020-10-13 18:00:22 -0400

    module 4 complete

--- schemas/pizza.js
+++ schemas/pizza.js
@@ -1,4 +1,5 @@
 import { MdLocalPizza as icon } from 'react-icons/md';
+import PriceInput from '../components/PriceInput';
 
 export default {
   name: 'pizza',
@@ -34,8 +35,35 @@ export default {
       title: 'Price',
       type: 'number',
       description: 'Price of the pizza in cents',
+      inputComponent: PriceInput,
       validation: (Rule) => Rule.min(1000),
-      //   inputComponent: PriceInput,
+      inputComponent: PriceInput,
+    },
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'topping' }] }],
     },
   ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    prepare: ({ title, media, ...toppings }) => {
+      // Filter undefined toppings out
+      const tops = Object.values(toppings).filter(Boolean);
+      // return da pizza
+      return {
+        title,
+        media,
+        subtitle: tops.join(', ').trimEnd(),
+      };
+    },
+  },
 };
